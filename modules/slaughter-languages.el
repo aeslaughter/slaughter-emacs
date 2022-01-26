d6e877c07604269a19afe9bce62340287798d5a1
Andrew E Slaughter <slaughter@coreform.com>
2022-01-26 16:58:55 -0700

    scribble mode

--- modules/slaughter-languages.el
+++ modules/slaughter-languages.el
@@ -2,5 +2,7 @@
 (slaughter-package-install 'markdown-mode)
 (slaughter-package-install 'racket-mode)
 
+(load-file "contrib/scribble-mode.el")
+
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
