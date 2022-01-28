65bad1c5e72538a746beb471c5b7e4439459170c
Andrew E Slaughter <slaughter@coreform.com>
2022-01-28 15:24:29 -0700

    use scribble-mode from melpa

--- modules/slaughter-languages.el
+++ modules/slaughter-languages.el
@@ -1,8 +1,11 @@
 (slaughter-package-install 'typescript-mode)
 (slaughter-package-install 'markdown-mode)
 (slaughter-package-install 'racket-mode)
+(slaughter-package-install 'scribble-mode)
 
-(load-file (concat this-directory "/modules/contrib/scribble-mode.el"))
+;;(load-file (concat this-directory "/modules/contrib/scribble-mode.el"))
 
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+
--- slaughter-emacs.el
+++ slaughter-emacs.el
@@ -55,3 +55,17 @@
 ;; navigation C-left, C-up, ... to C-h, C-k, ... to mimic vi
 ;; go to definition M-> C-< ???
 ;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (scribble-mode undo-tree typescript-mode smartparens seti-theme ripgrep racket-mode multiple-cursors markdown-mode magit helm flyspell-correct-ivy flycheck expand-region deft counsel-projectile company auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
