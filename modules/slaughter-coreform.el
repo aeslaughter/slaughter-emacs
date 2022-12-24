ee40ed8aed62e8a253e9f6dc8841a06cc2ee1a15
Andrew E Slaughter <slaughter@coreform.com>
2022-12-23 18:27:50 -0700

    working on getting ninja commands added

--- modules/slaughter-coreform.el
+++ modules/slaughter-coreform.el
@@ -12,21 +12,37 @@
       (ansi-color-apply-on-region compilation-filter-start (point-max))))
   (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
 
-
 (setq coreform-root-default (concat (getenv "HOME") "/cf/master/cf"))
 
-(defun coreform-get-root ()
+(defun coreform-get-root-dir ()
   "Coreform: check for a 'build' file in the version control root or use '~/cf/master/cf'."
   (if (file-exists-p (concat (vc-root-dir) "/build"))
       (vc-root-dir)
     coreform-root-default))
 
+(defun coreform-get-build-dir ()
+  "Coreform: return the build directory."
+  (defvar-local build-dir (cond ((string-equal "darwin" system-type) "build_mac64")
+                                ((string-prefix-p "windows" system-type) "build_win64")
+                                (t "build_lin64")))
+  (concat (coreform-get-root-dir)  "/../" build-dir))
+
+
 (defun coreform-build (build-type)
-  "Coreform: function for running 'build stage make'."
-  (let ((default-directory (coreform-get-root)))
+  "Coreform: run 'build stage make'."
+  (let ((default-directory (coreform-get-root-dir)))
     (defvar-local build-command (format "./build stage make build-type=%s" (symbol-name build-type)))
     (message build-command)
     (compile build-command)))
+
+(defun coreform-ninja (build-type)
+  "Coreform: run ninja"
+  (defvar ninja-command (cond ((eq build-type 'debug) "ninja -f build-Debug.ninja")
+                              ((eq build-type 'release) "ninja -f build-Release.ninja")
+                              (t "ninja -f build.ninja")))
+  (let ((default-directory (coreform-get-build-dir)))
+    (message ninja-command)
+    (compile ninja-command)))
 
 (defun coreform-build-debug ()
   "Coreform: configure and compile in debug mode."
@@ -38,11 +54,22 @@
   (interactive)
   (coreform-build 'release))
 
+(defun coreform-ninja-debug ()
+  "Coreform: execute ninja debug mode."
+  (interactive)
+  (coreform-ninja 'debug))
+
+(defun coreform-ninja-release ()
+  "Coreform: execute ninja release mode."
+  (interactive)
+  (coreform-ninja 'release))
+
 (defvar coreform-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "d" 'coreform-build-debug)
-    (define-key map "r" 'coreform-build-release)
+    (define-key map "b d" 'coreform-build-debug)
+    (define-key map "b r" 'coreform-build-release)
+    (define-key map "d" 'coreform-ninja-debug)
+    (define-key map "r" 'coreform-ninja-release)
     map)
   "Coreform: key map for Coreform utilities.")
 (local-set-key (kbd "C-f") coreform-map)
-
