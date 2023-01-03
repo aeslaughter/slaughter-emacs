5b5f2690c3e865cc14067e25f7b70e70ee4ca7ec
Andrew E Slaughter <slaughter@coreform.com>
2023-01-03 09:51:11 -0700

    get prefix key working

--- modules/slaughter-coreform.el
+++ modules/slaughter-coreform.el
@@ -1,6 +1,5 @@
 ;;; slaughter-coreform --- Utilities for building and running Coreform tools
 ;;; Commentary:
-;;; 
 
 ;; Enable color for *compilation* buffer
 (slaughter-package-install 'ansi-color)
@@ -12,14 +11,15 @@
       (ansi-color-apply-on-region compilation-filter-start (point-max))))
   (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
 
+;; function and default for the root coreform directory
 (setq coreform-root-default (concat (getenv "HOME") "/cf/master/cf"))
-
 (defun coreform-get-root-dir ()
   "Coreform: check for a 'build' file in the version control root or use '~/cf/master/cf'."
   (if (file-exists-p (concat (vc-root-dir) "/build"))
       (vc-root-dir)
     coreform-root-default))
 
+;; locate the build directory
 (defun coreform-get-build-dir ()
   "Coreform: return the build directory."
   (defvar-local build-dir (cond ((string-equal "darwin" (symbol-name system-type)) "build_mac64")
@@ -27,7 +27,7 @@
                                 (t "build_lin64")))
   (concat (coreform-get-root-dir)  "/../" build-dir))
 
-
+;; build state make
 (defun coreform-build (build-type)
   "Coreform: run 'build stage make'."
   (let ((default-directory (coreform-get-root-dir)))
@@ -35,6 +35,7 @@
     (message build-command)
     (compile build-command)))
 
+;; ninja only build
 (defun coreform-ninja (build-type)
   "Coreform: run ninja"
   (defvar ninja-command (cond ((eq build-type 'debug) "ninja -f build-Debug.ninja")
@@ -64,12 +65,11 @@
   (interactive)
   (coreform-ninja 'release))
 
-(defvar coreform-map
-  (let ((map (make-sparse-keymap)))
-    ;;(define-key map (kbd "C-d") 'coreform-build-debug)
-    ;;(define-key map (kbd "C-r") 'coreform-build-release)
-    (define-key map (kbd "d") 'coreform-ninja-debug)
-    (define-key map (kbd "r") 'coreform-ninja-release)
-    map)
-  "Coreform: key map for Coreform utilities.")
-(local-set-key (kbd "C-f") coreform-map)
+;; Create coreform map with prefix C-f
+(define-prefix-command 'coreform-map)
+(global-set-key "\C-f" 'coreform-map)
+
+(define-key coreform-map "d" 'coreform-ninja-debug)
+(define-key coreform-map "r" 'coreform-ninja-release)
+(define-key coreform-map "\C-d" 'coreform-build-debug)
+(define-key coreform-map "\C-r" 'coreform-build-release)
